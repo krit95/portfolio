9b9b7ef67fe87899df73e1031f8b90ff64e349fb
krit95 <goyal.kri@husky.neu.edu>
2019-10-07 14:29:37 -0400

    Update about detail

--- src/components/details/AboutDetails.js
+++ src/components/details/AboutDetails.js
@@ -1,7 +1,7 @@
 const AboutDetails = {
     img_src: "res/images/my_photo.jpg",
     subtitle : "Graduate Student | Android/Web developer | AI Enthusiast",
-    content : "Candidate for MS in Computer Science at Northeastern University. I am an avid learner and my latest obsession is Machine Learning and AI. I have extensive experience in Android and web development.",
+    content : "Candidate for MS in Computer Science at Northeastern University. I am an avid learner with extensive experience in Android and Web Development. Right now, I am also honing my skills in AI and ML by collaborating on some really interesting projects and ideas!",
     post_script: "Always looking for interesting opportunities to collaborate and learn in mobile/web development and Machine Learning."
 }
 
